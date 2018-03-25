282e098b96b111995189a15a5603d4bb76c8bd11
guihgs <guihqueiroz.15@gmail.com>
2018-03-25 12:05:48 -0300

    second comment

---
+++ footer.php
@@ -0,0 +1,15 @@
+<div class="row">
+    <div class="col-xs-12 text-center">
+<footer><p>&copy; AwesomeTheme <?php echo Date('Y'); ?></p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+     <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
+</footer>
+    </div>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
